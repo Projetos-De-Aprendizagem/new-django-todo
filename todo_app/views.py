f14f9002bfa37107e5de75621697cc8df683f3e7
EliasOPrado <eliasprado123@outlook.com>
2025-02-15 20:04:09 -0300

    Added pagination

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,25 +1,27 @@
 from django.shortcuts import redirect, get_object_or_404
+from django.core.paginator import Paginator
 from django.views.generic import FormView, ListView, View
 from django.urls import reverse_lazy
 from .models import Todo
 from .forms import TodoForm
 
 
-class TodoListView(FormView, ListView):
+class TodoListView(ListView, FormView):
     model = Todo
     template_name = 'todo_list.html'  # Custom template
     context_object_name = 'todos'  # Rename default "object_list"
     form_class = TodoForm
     success_url = reverse_lazy('todo-list')
     ordering = ['-id']  # Order tasks by newest first
+    paginate_by = 5 
 
     def form_valid(self, form):
-        form.save()  # Salva a nova tarefa
+        form.save()  # Save the new task
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['todos'] = Todo.objects.all()
+        context['form'] = self.get_form()  # Add the form to the context
         return context
 
     
